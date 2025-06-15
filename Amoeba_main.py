3db6bdce73fda4c95d8df067534286f71ef93ef1
Ran Tao <bobyellow315@gmail.com>
2025-06-15 14:57:24 -0400

    Update Amoeba_main.py

--- Amoeba_main.py
+++ Amoeba_main.py
@@ -41,12 +41,10 @@ y = dict(zip(df["O"], df["F"]))
 # Run AMOEBA with desired p-value threshold
 significance_level = 0.01
 #outputStr = execAMOEBA(y, neighbors, significance=significance_level)
-outputStr = execAMOEBA_headtail_fast(y, neighbors, significance=significance_level,mc_reps=500, headtail_pct=0.3)
+outputStr = execAMOEBA_headtail_fast(y, neighbors, significance=significance_level,mc_reps=500, headtail_pct=0.05)
 
 # Save output to file
-#output_path = "result/AMOEBA_County_Obesity_001sig_headtail_order_01.txt"
-output_path = "result/AMOEBA_Chicago_CT_GV_001sig_headtail_order_03.txt"
-#output_path = "result/AMOEBA_Weihai_001sig_headtail_01.txt"
+output_path = "result/AMOEBA_Chicago_CT_GV_sig001_headtail005.txt"
 # Ensure directory exists
 os.makedirs(os.path.dirname(output_path), exist_ok=True)
 with open(output_path, 'w') as f:
